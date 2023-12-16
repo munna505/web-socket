e846daceb9a560486e985ebf877f98ab1fce6ff8
munna <munna505>
2023-12-16 22:45:48 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import logo from './logo.svg';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
 import './App.css';
+import {useCallback } from 'react';
 
 function App() {
+  const WS_URL = 'wss://copyapi.jksconsultants.com/ws?symbol=AVXUSD';
+
+  const { sendMessage, lastJsonMessage,lastMessage, readyState } = useWebSocket(WS_URL,
+    {onOpen: () => console.log('opened'),
+  //Will attempt to reconnect on all close events, such as server shutting down
+    shouldReconnect: (closeEvent) => true,
+}
+    
+    );
+  
+  const handleClickSendMessage = useCallback(() => sendMessage('Hello'), []);
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: 'Connecting',
+    [ReadyState.OPEN]: 'Open',
+    [ReadyState.CLOSING]: 'Closing',
+    [ReadyState.CLOSED]: 'Closed',
+    [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
+  }[readyState];
+  console.log(lastMessage)
+  console.log(lastJsonMessage)
+  ;
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,8 +39,15 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
+        <button
+        onClick={handleClickSendMessage}
+        disabled={readyState !== ReadyState.OPEN}
+      >
+        Click Me to send 'Hello'
+      </button>
+      <span>The WebSocket is currently {connectionStatus}</span>
+      {lastMessage ? <span>Last message: {lastMessage.data}</span> : null}
       </header>
     </div>
   );
